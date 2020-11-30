872015d61bd9e46f878ca003d2cd8383fa689ef1
Artem-PC <talanov_artem@ukr.net>
2020-12-01 00:23:58 +0200

    01.12.2020

---
+++ src/sections/logo.php
@@ -0,0 +1,4 @@
+<div class="logo">
+    <img src="src/img/logo.png" alt="logo">
+    <h2>НАЦІОНАЛЬНИЙ УНІВЕРСИТЕТ <br> КОРАБЛЕБУДУВАННЯ <br> ІМЕНІ АДМІРАЛА МАКАРОВА</h2>
+</div>
--- src/sections/registration.php
+++ src/sections/registration.php
@@ -1,1 +1,20 @@
-<?php
+<section class="registration active">
+    <form action="" name="registration">
+        <div class="row">
+            <label for="name">Enter Your name</label>
+            <input id="name" type="text" placeholder="Name">
+        </div>
+
+        <div class="row">
+            <label for="lastName">Enter Your last name</label>
+            <input id="lastName" type="text" placeholder="Last Name">
+        </div>
+
+        <div class="row">
+            <label for="eMail">Enter Your e-mail</label>
+            <input id="eMail" type="email" placeholder="E-mail">
+        </div>
+
+        <button id="registration" type="button">Start the test</button>
+    </form>
+</section>
